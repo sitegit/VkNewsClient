2a28e73772871457fc54bb62cd0e3256673bae1c
Alexandr Pavlov <pavlov549@gmail.com>
2023-10-25 09:58:48 +0300

    create list and add remove post func

--- app/src/main/java/com/example/vknewsclient/MainViewModel.kt
+++ app/src/main/java/com/example/vknewsclient/MainViewModel.kt
@@ -8,11 +8,18 @@ import com.example.vknewsclient.domain.StatisticItem
 
 class MainViewModel : ViewModel() {
 
-    private val _feedPost = MutableLiveData(FeedPost())
-    val feedPost: LiveData<FeedPost> = _feedPost
+    private val sourceList = mutableListOf<FeedPost>().apply {
+        repeat(20) {
+            add(FeedPost(id = it))
+        }
+    }
 
-    fun updateCount(item: StatisticItem) {
-        val oldStatistics = _feedPost.value?.statistics ?: throw IllegalStateException("FeedPost == null")
+    private val _feedPosts = MutableLiveData<List<FeedPost>>(sourceList)
+    val feedPost: LiveData<List<FeedPost>> = _feedPosts
+
+    fun updateCount(feedPost: FeedPost, item: StatisticItem) {
+        val oldPosts = _feedPosts.value?.toMutableList() ?: mutableListOf()
+        val oldStatistics = feedPost.statistics
         val newStatistics = oldStatistics.toMutableList().apply {
             replaceAll { oldItem ->
                 if (oldItem.type == item.type) {
@@ -22,6 +29,21 @@ class MainViewModel : ViewModel() {
                 }
             }
         }
-        _feedPost.value = _feedPost.value?.copy(statistics = newStatistics)
+        val newFeedPost = feedPost.copy(statistics = newStatistics)
+        _feedPosts.value = oldPosts.apply {
+            replaceAll {
+                if (it.id == newFeedPost.id) {
+                    newFeedPost
+                } else {
+                    it
+                }
+            }
+        }
+    }
+
+    fun removePost(feedPost: FeedPost) {
+        val oldPosts = _feedPosts.value?.toMutableList() ?: mutableListOf()
+        oldPosts.remove(feedPost)
+        _feedPosts.value = oldPosts
     }
 }
--- app/src/main/java/com/example/vknewsclient/ui/theme/VKNewsMainScreen.kt
+++ app/src/main/java/com/example/vknewsclient/ui/theme/VKNewsMainScreen.kt
@@ -1,7 +1,12 @@
 package com.example.vknewsclient.ui.theme
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.DismissDirection
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -9,7 +14,9 @@ import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SwipeToDismiss
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDismissState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableIntStateOf
@@ -18,13 +25,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.vknewsclient.MainViewModel
-import com.example.vknewsclient.domain.FeedPost
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
 fun MainScreen(viewModel: MainViewModel) {
-
     Scaffold(
         bottomBar = {
             NavigationBar {
@@ -58,17 +63,49 @@ fun MainScreen(viewModel: MainViewModel) {
             }
         },
         content = {
-            val feedPost = viewModel.feedPost.observeAsState(FeedPost())
-            PostCard(
-                modifier = Modifier.padding(8.dp),
-                feedPost = feedPost.value,
-                onViewsClickListener = viewModel::updateCount,
-                onLikeClickListener = viewModel::updateCount,
-                onShareClickListener = viewModel::updateCount,
-                onCommentClickListener = { newItem ->
-                    viewModel.updateCount(newItem)
-                } //viewModel::updateCount
-            )
+            val feedPosts = viewModel.feedPost.observeAsState(listOf())
+
+            LazyColumn(
+                contentPadding = PaddingValues(
+                    top = 16.dp,
+                    start = 8.dp,
+                    end = 8.dp,
+                    bottom = 92.dp
+                ),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                items(items = feedPosts.value, key = { it.id }) {feedPost ->
+                    val dissmissState = rememberDismissState()
+
+                    if (dissmissState.isDismissed(direction = DismissDirection.EndToStart)) {
+                        viewModel.removePost(feedPost)
+                    }
+
+                    SwipeToDismiss(
+                        modifier = Modifier.animateItemPlacement(),
+                        state = dissmissState,
+                        background = {},
+                        directions = setOf(DismissDirection.EndToStart),
+                        dismissContent = {
+                            PostCard(
+                                feedPost = feedPost,
+                                onViewsClickListener = { statisticItem ->
+                                    viewModel.updateCount(feedPost, statisticItem)
+                                },
+                                onLikeClickListener = { statisticItem ->
+                                    viewModel.updateCount(feedPost, statisticItem)
+                                },
+                                onCommentClickListener = { statisticItem ->
+                                    viewModel.updateCount(feedPost, statisticItem)
+                                },
+                                onShareClickListener = { statisticItem ->
+                                    viewModel.updateCount(feedPost, statisticItem)
+                                }
+                            )
+                        }
+                    )
+                }
+            }
         }
     )
 }
