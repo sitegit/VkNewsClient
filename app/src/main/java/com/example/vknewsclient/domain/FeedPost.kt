be04ae10f05fb34010adf5a89760548d87fa4e29
Alexandr Pavlov <pavlov549@gmail.com>
2023-10-27 16:15:26 +0300

    Passing parameters (Parcelable and Custom Nav Type ver.)

--- app/src/main/java/com/example/vknewsclient/domain/FeedPost.kt
+++ app/src/main/java/com/example/vknewsclient/domain/FeedPost.kt
@@ -1,6 +1,13 @@
 package com.example.vknewsclient.domain
 
+import android.os.Bundle
+import android.os.Parcelable
+import androidx.navigation.NavType
 import com.example.vknewsclient.R
+import com.google.gson.Gson
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class FeedPost(
     val id: Int = 0,
     val communityName: String = "/dev/null",
@@ -14,4 +21,21 @@ data class FeedPost(
         StatisticItem(type = StatisticType.COMMENTS, 8),
         StatisticItem(type = StatisticType.LIKES, 27)
     )
-)
+) : Parcelable {
+    companion object {
+
+        val NavigationType: NavType<FeedPost> = object : NavType<FeedPost>(false) {
+            override fun get(bundle: Bundle, key: String): FeedPost? {
+                return bundle.getParcelable(key)
+            }
+
+            override fun parseValue(value: String): FeedPost {
+                return Gson().fromJson(value, FeedPost::class.java)
+            }
+
+            override fun put(bundle: Bundle, key: String, value: FeedPost) {
+                bundle.putParcelable(key, value)
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/vknewsclient/navigation/HomeScreenNavGraph.kt
+++ app/src/main/java/com/example/vknewsclient/navigation/HomeScreenNavGraph.kt
@@ -2,13 +2,11 @@ package com.example.vknewsclient.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavGraphBuilder
-import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import androidx.navigation.navigation
 import com.example.vknewsclient.domain.FeedPost
 import com.example.vknewsclient.navigation.Screen.Companion.KEY_FEED_POST
-import com.google.gson.Gson
 
 fun NavGraphBuilder.homeScreenNavGraph(
     newsFeedScreenContent: @Composable () -> Unit,
@@ -25,12 +23,11 @@ fun NavGraphBuilder.homeScreenNavGraph(
             route = Screen.Comments.route,
             arguments = listOf(
                 navArgument(name = KEY_FEED_POST) {
-                    type = NavType.StringType
+                    type = FeedPost.NavigationType
                 }
             )
         ) {
-            val feedPostJson = it.arguments?.getString(KEY_FEED_POST) ?: ""
-            val feedPost = Gson().fromJson(feedPostJson, FeedPost::class.java)
+            val feedPost = it.arguments?.getParcelable<FeedPost>(KEY_FEED_POST) ?: throw RuntimeException("Args is null")
             commentsScreenContent(feedPost)
         }
     }
